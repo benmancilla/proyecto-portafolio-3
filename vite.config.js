01cff0fabcc3f48e745dcf3f8b68c35d9ffa3eed
benmancilla <ben.mancilla@duocuc.cl>
2024-05-21 19:18:44 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pagina-portafolio-3/', // Update this with your repository name
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 });
+
